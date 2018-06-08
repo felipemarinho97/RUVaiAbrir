ae1dc4af3b636bf58883834fa9e63976e6db6576
Felipe Marinho <felipevm97@gmail.com>
2018-06-08 19:56:11 -0300

    Create SpringBoot backend basic stucture

---
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/RuVaiAbrirApplication.java
@@ -0,0 +1,12 @@
+package ml.darklyn.RUVaiAbrir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuVaiAbrirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuVaiAbrirApplication.class, args);
+	}
+}
