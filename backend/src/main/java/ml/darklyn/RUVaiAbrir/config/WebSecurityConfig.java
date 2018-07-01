6621cf8d202b7cd39b92d2ab52a3da7b547334de
Felipe Marinho <felipevm97@gmail.com>
2018-07-01 15:10:06 -0300

    add security config

---
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/config/WebSecurityConfig.java
@@ -0,0 +1,108 @@
+package ml.darklyn.RUVaiAbrir.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import ml.darklyn.RUVaiAbrir.config.jwt.JwtAuthenticationEntryPoint;
+import ml.darklyn.RUVaiAbrir.config.jwt.JwtAuthenticationFilter;
+import ml.darklyn.RUVaiAbrir.config.jwt.JwtTokenProvider;
+import ml.darklyn.RUVaiAbrir.user.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+	
+	@Value("${app.reactJSClientOrigin}")
+	private final String REACTJS_CLIENT_ORIGIN = null;
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.cors().and().csrf().disable()
+			.exceptionHandling()
+				.authenticationEntryPoint(unauthorizedHandler)
+				.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/auth/register").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+				.and()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.PUT, "/status", "/cardapio").hasRole("FUNCIONARIO")
+				.and()
+			.authorizeRequests()
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.and()
+			.authorizeRequests()
+				.anyRequest().authenticated();
+		
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+			
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return passwordEncoder;
+	}
+	
+	@Bean
+	public CustomUserDetailsService customUserDetailsService() {
+		return new CustomUserDetailsService();
+	}
+	
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+	
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	public JwtTokenProvider jwtTokenProvider() {
+		return new JwtTokenProvider();
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder
+			.userDetailsService(customUserDetailsService())
+			.passwordEncoder(passwordEncoder());
+	}
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(REACTJS_CLIENT_ORIGIN);
+    }
+	
+
+}
