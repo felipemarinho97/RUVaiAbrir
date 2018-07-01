99ebf824c76b75a1059cd1f5ab002fb7ff92afad
Felipe Marinho <felipevm97@gmail.com>
2018-07-01 15:13:50 -0300

    implementig a customUserDetails Service and userPrincipal transient abstraction

---
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/user/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package ml.darklyn.RUVaiAbrir.user;
+
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	private static final String USERNAME_NOT_FOUND_MESSAGE = "O usuário requesitado não foi encontrado.";
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {		
+		  User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+				  .orElseThrow(() -> new UsernameNotFoundException(USERNAME_NOT_FOUND_MESSAGE));
+		  return UserPrincipal.create(user);
+	}
+	
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new UsernameNotFoundException(USERNAME_NOT_FOUND_MESSAGE));
+		return UserPrincipal.create(user);
+	}
+
+}
