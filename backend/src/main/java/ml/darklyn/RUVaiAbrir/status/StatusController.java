948a39747cf04965a5775eb0f248e7782d8a6725
Felipe Marinho <felipevm97@gmail.com>
2018-07-06 11:54:00 -0300

    refactor rest endpoints to usuario-status #4

--- backend/src/main/java/ml/darklyn/RUVaiAbrir/status/StatusController.java
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/status/StatusController.java
@@ -7,15 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import ml.darklyn.RUVaiAbrir.dto.StatusDTO;
 import ml.darklyn.RUVaiAbrir.user.UserPrincipal;
@@ -42,16 +41,14 @@ public class StatusController {
 	}
 	
 	
-	
-	@GetMapping("/usuario/status")
-	public ResponseEntity<?> getUserStatus() {
+	@GetMapping("/usuario-status")
+	public ResponseEntity<?> getUserStatus(@RequestParam(required=false, defaultValue="average") String type) {
 		Status status = statusService.getCurrentUserStatus();
 		
 		return ResponseEntity.ok(status);
 	}
 	
-	
-	@PostMapping("/usuario/status")
+	@PostMapping("/usuario-status")
 	public ResponseEntity<?> createUserStatus(@Valid @RequestBody StatusDTO statusDTO, 
 			@AuthenticationPrincipal UserPrincipal userPrincipal) {
 		UserStatus status = statusService.createUserStatus(statusDTO, userPrincipal.getEmail());
@@ -59,14 +56,14 @@ public class StatusController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(status);
 	}
 	
-	@GetMapping("/usuario/status/{id}")
+	@GetMapping("/usuario-status/{id}")
 	public ResponseEntity<?> getUserStatusById(@PathVariable("id") Long id) {
 		UserStatus status = statusService.getUserStatusById(id);
 		
 		return ResponseEntity.ok(status);
 	}
 	
-	@PutMapping("/usuario/status/{id}")
+	@PutMapping("/usuario-status/{id}")
 	public ResponseEntity<?> updateUserStatus(
 			@PathVariable("id") Long id, 
 			@Valid @RequestBody StatusDTO statusDTO) {
@@ -75,7 +72,7 @@ public class StatusController {
 		return ResponseEntity.ok(status);
 	}
 	
-	@DeleteMapping("/usuario/status/{id}")
+	@DeleteMapping("/usuario-status/{id}")
 	public ResponseEntity<?> removeUserStatus(
 			@PathVariable("id") Long id,
 			@AuthenticationPrincipal UserPrincipal userPrincipal) {
