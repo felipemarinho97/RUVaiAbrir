59e443008f252df67c9e766c4cd71420fdb3f4d5
Felipe Marinho <felipevm97@gmail.com>
2018-07-06 11:56:45 -0300

    ignore critical fields on json serialization

--- backend/src/main/java/ml/darklyn/RUVaiAbrir/user/UserRepository.java
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/user/UserRepository.java
@@ -2,13 +2,8 @@ package ml.darklyn.RUVaiAbrir.user;
 
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import ml.darklyn.RUVaiAbrir.config.JdbcRepository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
