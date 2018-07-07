9380a299e22b8f1bf3f7872e61f2c51c3f51d356
Felipe Marinho <felipevm97@gmail.com>
2018-07-06 23:18:20 -0300

    update MealType enum

--- backend/src/main/java/ml/darklyn/RUVaiAbrir/enumeration/MealType.java
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/enumeration/MealType.java
@@ -1,16 +1,24 @@
 package ml.darklyn.RUVaiAbrir.enumeration;
 
 public enum MealType {
-	LUNCH("Almoço"),
-	DINNER("Janta");
+	LUNCH("Almoço", "almoco"),
+	DINNER("Janta", "janta");
 	
 	private String customName;
 	
-	MealType(String customName) {
+	private String paramName;
+	
+	MealType(String customName, String paramName) {
 		this.customName = customName;
+		this.paramName = paramName;
 	}
 
 	public String getCustomName() {
 		return customName;
 	}
+
+	public String getParamName() {
+		return paramName;
+	}
+	
 }
