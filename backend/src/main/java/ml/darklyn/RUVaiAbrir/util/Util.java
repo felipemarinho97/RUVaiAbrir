f510f74cf0a98dd259f96456a5ecfd9626255953
Felipe Marinho <felipevm97@gmail.com>
2018-07-08 00:56:21 -0300

    create userStatus inc for comments

--- backend/src/main/java/ml/darklyn/RUVaiAbrir/util/Util.java
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/util/Util.java
@@ -9,6 +9,8 @@ import ml.darklyn.RUVaiAbrir.comments.Comment;
 import ml.darklyn.RUVaiAbrir.exceptions.BadRequestException;
 import ml.darklyn.RUVaiAbrir.rating.Rating;
 import ml.darklyn.RUVaiAbrir.rating.RatingService;
+import ml.darklyn.RUVaiAbrir.status.StatusService;
+import ml.darklyn.RUVaiAbrir.status.UserStatus;
 
 @Component
 public class Util {
@@ -16,12 +18,17 @@ public class Util {
 	@Autowired
 	private RatingService ratingService;
 	
+	@Autowired
+	private StatusService statusService;
+	
 	public void applyIncludeParams(List<String> include, final Comment comment) {
 		include.forEach((toInclude) -> {
 			switch (toInclude) {
 				case "classificacao":
 					applyRatingParam(comment);
 					break;
+				case "status":
+					applyUserStatusParam(comment);
 				default:
 					throw new BadRequestException("O atributo '"+toInclude+"' passado para o parâmetro 'inc' é inválido.");
 			}
@@ -32,6 +39,15 @@ public class Util {
 		try {
 			Rating rating = ratingService.getRating(comment.getUser(), comment.getDate(), comment.getMealType());
 			comment.setRating(rating.getRating());
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	private void applyUserStatusParam(final Comment comment) {
+		try {
+			UserStatus userStatus = statusService.getUserStatus(comment.getUser(), comment.getDate(), comment.getMealType());
+			comment.setUserStatus(userStatus);
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
