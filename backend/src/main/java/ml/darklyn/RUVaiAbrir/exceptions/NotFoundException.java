aa50e413a91ff571a9d6e26ce9e7bcbde2ecfd98
Felipe Marinho <felipevm97@gmail.com>
2018-07-07 22:27:32 -0300

    updating NOtFoundException

--- backend/src/main/java/ml/darklyn/RUVaiAbrir/exceptions/NotFoundException.java
+++ backend/src/main/java/ml/darklyn/RUVaiAbrir/exceptions/NotFoundException.java
@@ -14,6 +14,7 @@ public class NotFoundException extends RuntimeException {
 	public NotFoundException(String message) {
 		super(message);
 	}
+
 	
 	public NotFoundException(String message, Throwable cause) {
 		super(message, cause);
