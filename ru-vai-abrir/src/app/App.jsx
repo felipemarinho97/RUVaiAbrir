b2f6a4c5b75e2988bf28870d2ef7e052b598b9ef
Felipe Marinho <felipevm97@gmail.com>
2018-07-10 19:40:39 -0300

    layout finalizado

--- ru-vai-abrir/src/app/App.jsx
+++ ru-vai-abrir/src/app/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './food-icon-1.svg';
 import './App.css';
-import { Layout, Button, Icon } from 'antd';
+import { Layout, Button, Icon, Menu } from 'antd';
 
 const { Header, Content, Footer } = Layout;
+const { Item } = Menu;
+
+const APP_NAME = "RU Vai Abrir?"
 
 class App extends Component {
   
@@ -12,14 +15,19 @@ class App extends Component {
       <Layout className="App-container">
         <Header className="App-header">
           <div className="App-logo">
-            <Icon type="clock-circle-o" style={{ fontSize:18 }}/> Ru Vai Abrir?
+            <img src={logo} alt="logo" /> <span className="App-logo-name">{APP_NAME}</span>
           </div>
+          <Menu mode="horizontal" style={{ lineHeight: '63px', float:'right', borderBottom:'none' }}>
+            <Item key="1">Login</Item>
+          </Menu>
         </Header>
         <Content>
-          Lorem Ipsum
+          
         </Content>
         <Footer className="App-footer">
-          <div className="App-footer-copyright">RuVaiAbrir? <Icon type="copyright" /> Criado por Felipe Marinho</div>
+          <div className="App-footer-copyright">
+            {APP_NAME} <Icon type="copyright" /> Desenvolvido com <Icon type="heart" style={{ fontSize:10 }} /> por Felipe Marinho
+          </div>
         </Footer>
       </Layout>
     );
