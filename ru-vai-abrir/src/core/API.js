29e5751d1a0a087aa9d12be8b06ddf6454edd84a
Felipe Marinho <felipevm97@gmail.com>
2018-07-18 11:51:31 -0300

    Novos métodos de acesso da API

--- ru-vai-abrir/src/core/API.js
+++ ru-vai-abrir/src/core/API.js
@@ -5,7 +5,7 @@ import Auth from './auth/Auth';
 
 class API extends Component {
 
-    static signedGetCall(endpoint, options) {
+    static signedCall(method, endpoint, body, options = {}) {
         const token = Auth.getUserAuthToken();
         if (!token)
             return new Promise((_, rej) => rej("O usuário deve estar logado para ter acesso ao sistema!"));
@@ -15,15 +15,60 @@ class API extends Component {
             }
         }, options);
 
-        return axios.get(API_BASE_URL + endpoint, requestOptions);
+        if (body)
+            return axios[method](API_BASE_URL + endpoint, body, requestOptions);
+        else 
+            return axios[method](API_BASE_URL + endpoint, requestOptions);
     }
 
+    // static signedDeleteCall(endpoint, options = {}) {
+    //     const token = Auth.getUserAuthToken();
+    //     if (!token)
+    //         return new Promise((_, rej) => rej("O usuário deve estar logado para ter acesso ao sistema!"));
+    //     let requestOptions = Object.assign({
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         }
+    //     }, options);
+
+    //     return axios.delete(API_BASE_URL + endpoint, requestOptions);
+    // }
+
+    // static signedPutCall(endpoint, comment, options = {}) {
+    //     const token = Auth.getUserAuthToken();
+    //     if (!token)
+    //         return new Promise((_, rej) => rej("O usuário deve estar logado para ter acesso ao sistema!"));
+    //     let requestOptions = Object.assign({
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         }
+    //     }, options);
+
+    //     return axios.put(API_BASE_URL + endpoint, comment, requestOptions);
+    // }
+
     static getCurrentStatus() {
-        return this.signedGetCall('/status');
+        return this.signedCall('get','/status');
     }
 
     static getCurrentUserStatus() {
-        return this.signedGetCall('/usuario-status');
+        return this.signedCall('get', '/usuario-status');
+    }
+
+    static getComments(pagina, limite, inc) {
+        return this.signedCall('get', '/comentarios');
+    }
+
+    static updateComment(id, comment) {
+        return this.signedCall('put', '/comentario/' + id, comment);
+    }
+
+    static removeComment(id) {
+        return this.signedCall('delete', '/comentario/' + id);
+    }
+
+    static addComment(comment) {
+        return this.signedCall('post', '/comentario', comment);
     }
 }
 
