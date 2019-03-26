02dae0244304b6a3f7f661a8e074f96a715fa6a1
Felipe Marinho <felipevm97@gmail.com>
2019-03-26 08:44:56 -0300

    fix time bug

--- ru-vai-abrir/src/comments/Comment.js
+++ ru-vai-abrir/src/comments/Comment.js
@@ -6,22 +6,24 @@ import API from '../core/API';
 const { TextArea } = Input;
 
 const RemoveAction = ({ confirmAction }) => (
-    <Tooltip placement="bottom" title={"Remover"}>
-        <Popconfirm title="Remover comentário?" placement="right"
+    <Tooltip placement="bottom" title={'Remover'}>
+        <Popconfirm
+            title="Remover comentário?"
+            placement="right"
             onConfirm={confirmAction}
-            okText="Sim" cancelText="Não">
+            okText="Sim"
+            cancelText="Não"
+        >
             <Icon type="delete" />
-        </ Popconfirm>
+        </Popconfirm>
     </Tooltip>
 );
 
 const EditAction = ({ setEditing }) => (
-    <Tooltip placement="bottom" title={"Editar"}>
-        <Icon
-            onClick={setEditing}
-            type="edit" />
+    <Tooltip placement="bottom" title={'Editar'}>
+        <Icon onClick={setEditing} type="edit" />
     </Tooltip>
-)
+);
 
 class Comment extends Component {
     constructor(props) {
@@ -29,62 +31,73 @@ class Comment extends Component {
 
         this.state = {
             isEditing: false,
-            message: this.props.item.message
+            message: this.props.item.message,
         };
 
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e) {
-        this.setState({ message: e.target.value })
+        this.setState({ message: e.target.value });
     }
 
     updateComment(id) {
         const comment = {
-            message: this.state.message
-        }
+            message: this.state.message,
+        };
         API.updateComment(id, comment).then(res => {
-            this.setState({ isEditing: false, message: res.data.message});
+            this.setState({ isEditing: false, message: res.data.message });
             console.log(res);
-            
-        })
-        
+        });
     }
 
     removeComment(id) {
-        API.removeComment(id).then(() => {
-            this.props.removeFromComments(id);
-        }).catch(err => {
-            console.log(err.response);
-
-        })
-
+        API.removeComment(id)
+            .then(() => {
+                this.props.removeFromComments(id);
+            })
+            .catch(err => {
+                console.log(err.response);
+            });
     }
 
     render() {
         const item = this.props.item;
 
         return (
-            <List.Item 
-                actions={[<EditAction setEditing={() => this.setState({ isEditing: true })} />, <RemoveAction confirmAction={() => this.removeComment(item.id)} />]}
-                extra={<Rate disabled defaultValue={item.rating} />} >
+            <List.Item
+                actions={[
+                    <EditAction
+                        setEditing={() => this.setState({ isEditing: true })}
+                    />,
+                    <RemoveAction
+                        confirmAction={() => this.removeComment(item.id)}
+                    />,
+                ]}
+                extra={<Rate disabled defaultValue={item.rating} />}
+            >
                 <List.Item.Meta
                     avatar={<Avatar>{item.user.firstName[0]}</Avatar>}
-                    title={[<Avatar style={{ marginRight: '1rem' }}>{item.user.firstName[0]}</Avatar>, <Link to={`/user/${item.user.username}`}>{item.user.firstName}</Link>]}
-                    description={item.user.email} />
-                {
-                    !this.state.isEditing ?
-                        (<div>{this.state.message}</div>)
-                    : (<TextArea
-                            value={this.state.message}
-                            onChange={this.handleChange}
-                            onPressEnter={() => this.updateComment(item.id)}
-                            maxLength={255} autosize={{ minRows: 2, maxRows: 6 }} />)
-                }
-
-
+                    title={[
+                        <Link to={`/user/${item.user.username}`}>
+                            {item.user.firstName}
+                        </Link>,
+                    ]}
+                    description={item.user.email}
+                />
+                {!this.state.isEditing ? (
+                    <div>{this.state.message}</div>
+                ) : (
+                    <TextArea
+                        value={this.state.message}
+                        onChange={this.handleChange}
+                        onPressEnter={() => this.updateComment(item.id)}
+                        maxLength={255}
+                        autosize={{ minRows: 2, maxRows: 6 }}
+                    />
+                )}
             </List.Item>
-        )
+        );
     }
 }
 
